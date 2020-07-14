5af9b3e2be0f7279ac8f49cc636caecbe691499c
Oscar <ommalor@gmail.com>
2020-07-14 12:09:30 -0400

    Initial Commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using MvcBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            var book = new Book();
+            return View();
+        }
+    }
+}
